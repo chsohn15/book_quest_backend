467bb7a71b20ae429c3f3c6532ed511065d0dcc4
chsohn15 <chsohn15@gmail.com>
2020-10-17 00:34:31 -0400

    add to rewards create method

--- app/controllers/api/v1/rewards_controller.rb
+++ app/controllers/api/v1/rewards_controller.rb
@@ -15,15 +15,22 @@ class Api::V1::RewardsController < ApplicationController
     def create 
         reward_tier_id = RewardTier.find_by(level: params[:level]).id
 
-        reward = Reward.create(
-            price: params[:price], 
-            student_id: params[:student_id], 
-            description: params[:description], 
-            reward_tier_id: reward_tier_id,
-            redeemed: true)
+        user = User.find_by(id: params[:student_id])
 
-        render json: reward
+        description = params[:description]
 
+        if user.rewards_hash[description]
+            render json: {errors: "You've already redeemed this reward!"}
+        else
+            reward = Reward.create(
+                price: params[:price], 
+                student_id: params[:student_id], 
+                description: params[:description], 
+                reward_tier_id: reward_tier_id,
+                redeemed: true)
+
+            render json: reward
+        end
     end
 
 
