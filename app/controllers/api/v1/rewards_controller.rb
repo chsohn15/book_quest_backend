e52b953e5b764719b8895c01d4da7fb06a0a116e
chsohn15 <chsohn15@gmail.com>
2020-10-20 14:54:26 -0400

    add alerts for awards

--- app/controllers/api/v1/rewards_controller.rb
+++ app/controllers/api/v1/rewards_controller.rb
@@ -13,16 +13,17 @@ class Api::V1::RewardsController < ApplicationController
     end
 
     def create 
-        reward_tier_id = RewardTier.find_by(level: params[:level]).id
+        level = params[:level]
+        reward_tier_id = RewardTier.find_by(level: level).id
 
         user = User.find_by(id: params[:student_id])
 
         description = params[:description]
 
         price = params[:price]
-        if reward_tier_id == 2 && user.tier_one_complete == false 
+        if level == 2 && user.tier_one_complete == false 
             render json: {errors: "You need to redeem Level 1 rewards first!"}
-        elsif reward_tier_id == 3 && user.tier_two_complete == false 
+        elsif level == 3 && user.tier_two_complete == false 
             render json: {errors: "You need to redeem Level 2 rewards first!"}
         elsif user.rewards_hash[description]
             render json: {errors: "You've already redeemed this reward!"}
