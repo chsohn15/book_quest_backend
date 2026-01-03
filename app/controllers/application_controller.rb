29f1b2c27c82477b67f4e8fc07fad7e8f9fb029a
chsohn15 <chsohn15@gmail.com>
2026-01-03 02:23:05 -0500

    update logged in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,23 @@
 class ApplicationController < ActionController::API
-    before_action :logged_in? 
+    before_action :logged_in?, except: [:index, :show]
 
     def encode_token(payload)
         JWT.encode(payload, "copperfield")
     end 
 
-    def logged_in? 
+    def logged_in?
         headers = request.headers["Authorization"]
+        return render json: {error: "Please login"} unless headers
+        
         token = headers.split(" ")[1]
-
-        begin user_id = JWT.decode(token, "copperfield")[0]["user_id"]
-
-            user = User.find(user_id)
-        rescue 
-
-            user = nil 
-        end 
+        
+        begin
+          user_id = JWT.decode(token, "copperfield")[0]["user_id"]
+          user = User.find(user_id)
+        rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+          user = nil
+        end
+        
         render json: {error: "Please login"} unless user
     end
 end
